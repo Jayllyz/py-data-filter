7eb9116a17140368a71dded3230e269ab532b431
Simon ZHANG <102216594+userMeh@users.noreply.github.com>
2024-06-22 12:11:30 +0200

    feat: retrieve input file (#2)
    * feat: retrieve input file
    
    * feat: read csv
    
    * fix: import version
    
    * fix: import version
    
    * fix: import version
    
    * fix: i give up
    
    * fix: format
    
    * fix: format

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-def function():
-    print("Hello, World!")
-
+from PyQt5 import QtWidgets
+from src.ui.dialog import Ui_MainWindow
+from src.setup import Setup
+import sys
 
 if __name__ == "__main__":
-    function()
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+
+    Setup(MainWindow, ui).setup()
+
+    MainWindow.show()
+    sys.exit(app.exec_())
