4b04d9136d05282a8598ee28de3e2633b4f6b713
Simon ZHANG <102216594+userMeh@users.noreply.github.com>
2024-06-22 19:35:10 +0200

    feat: yml and xml

--- src/data.py
+++ src/data.py
@@ -1,5 +1,7 @@
 import json
 import csv
+import xml.etree.ElementTree as xml
+import yaml
 
 
 class Data:
@@ -13,6 +15,10 @@ class Data:
             return self.json_file(file)
         elif ext == "csv":
             return self.csv_file(file)
+        elif ext == "xml":
+            return self.xml_file(file)
+        elif ext == "yml" or ext == "yaml":
+            return self.yml_file(file)
 
     def json_file(self, file: str):
         data = {}
@@ -37,6 +43,31 @@ class Data:
                             self.correct_type(v) for v in value.split(",")
                         ]
                 data[file_name].append(array)
+        return data
+
+    def xml_file(self, file: str):
+        tree = xml.parse(file)
+        root = tree.getroot()
+        name = root[0].tag
+
+        data = {name: []}
+
+        for child in root:
+            array = {}
+            for sub_child in child:
+                if len(sub_child) > 0:
+                    array[sub_child.tag] = [
+                        self.correct_type(sub_sub_child.text)
+                        for sub_sub_child in sub_child
+                    ]
+                else:
+                    array[sub_child.tag] = self.correct_type(sub_child.text)
+            data[name].append(array)
+        return data
+
+    def yml_file(self, file: str):
+        with open(file, "r") as f:
+            data = yaml.load(f, Loader=yaml.FullLoader)
         return data
 
     def correct_type(self, value: str):
