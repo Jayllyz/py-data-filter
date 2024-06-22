ee0fa41d82e60e6cbae862dcda8c00cb8ae31c34
Simon ZHANG <102216594+userMeh@users.noreply.github.com>
2024-06-22 11:58:29 +0200

    feat: read csv

--- src/setup.py
+++ src/setup.py
@@ -1,5 +1,6 @@
-from PyQt5.QtWidgets import QMainWindow, QDialog, QFileDialog
+from PyQt5.QtWidgets import QMainWindow, QFileDialog, QMessageBox
 from src.ui.dialog import Ui_MainWindow
+from src.data import Data
 import json
 import sys
 
@@ -8,12 +9,22 @@ class Setup(Ui_MainWindow):
         super().__init__()
         self.ui = ui
         self.MainWindow = MainWindow
+        self.data = {}
 
     def setup(self):
-        main_container = self.MainWindow.centralWidget()
         self.ui.buttonFolder.clicked.connect(lambda: self.select_file())
+        self.ui.buttonData.clicked.connect(lambda: self.process_data())
 
     def select_file(self):
         file, _ = QFileDialog.getOpenFileName(self.MainWindow, "Sélectionner un fichier", "", "Fichiers json (*.json);;Fichiers csv (*.csv)")
         if file:
             self.ui.inputFolder.setText(file)
+
+    def process_data(self):
+        if not self.ui.inputFolder.text():
+            QMessageBox.warning(self.MainWindow, "Erreur", "Veuillez sélectionner un fichier")
+            return
+        self.ui.dataOutput.clear()
+        self.data = Data().process(self.ui.inputFolder.text())
+        print(self.data)
+        self.ui.dataOutput.setPlainText(json.dumps(self.data, indent=4))
